92c59c9b41f3586ce463e338b2d264c5558b6e4d
a1729200350 <a1729200350@gmail.com>
2026-08-25 18:10:44 +0800

    更新运动学监控代码

--- src/two_link_arm_kinematics/two_link_arm_kinematics/kinematics_monitor.py
+++ src/two_link_arm_kinematics/two_link_arm_kinematics/kinematics_monitor.py
@@ -1,14 +1,11 @@
 """Monitor planar three-link arm kinematics and velocity mappings."""
-
 import math
-
+from scipy.optimize import lsq_linear
 from geometry_msgs.msg import Twist
 import numpy as np
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
-
-
 class KinematicsMonitor(Node):
     """Monitor forward kinematics and inverse-velocity solutions."""
 
@@ -205,6 +202,41 @@ class KinematicsMonitor(Node):
             0.01,
             0.5
         ))
+        # -----------------------------------------
+        #   简单线性激活
+        # -----------------------------------------
+        
+        d_safe = 1.0
+        d_danger = 0.3
+        # #定义最简单任务激活系数  
+        # #α=(d_safe-d_min)/d_safe-d_danger
+        # activation = (
+        #     (d_safe - nearest_distance)
+        #     / (d_safe - d_danger)
+        # )
+
+        # activation = float(
+        #     np.clip(activation, 0.0, 1.0)
+        # )
+        #控制器余弦平滑处理任务激活系数
+        #s=(d_safe-d_min)/d_safe-d_danger
+        #α=0.5[1-cos(πs)]  s∈[0,1]
+        # d_min >= d_safe   -> α = 0
+        # d_min <= d_danger -> α = 1
+        # 中间区域           -> 0 < α < 1
+        # -----------------------------------------
+        s = (
+            (d_safe - nearest_distance)
+            / (d_safe - d_danger)
+        )
+        #将 s限制在0 ~ 1之间 防止因为余弦周期性得到错误数值 
+        s = float(
+            np.clip(s, 0.0, 1.0)
+        )
+
+        activation = 0.5 * (
+            1.0 - math.cos(math.pi * s)
+        )
         #只输出z_limit更改
         # secondary_velocity = -gain * joint_limit_gradient
         # null_space_projector = np.eye(3) - jacobian_pinv @ jacobian
@@ -215,9 +247,9 @@ class KinematicsMonitor(Node):
         # if max_value > max_speed:
         #     null_space_velocity *= max_speed / max_value
 
-        z_limit = -gain * joint_limit_gradient
+        z_limit = -activation*gain * joint_limit_gradient
 
-        return z_limit,gain, nearest_distance
+        return z_limit,gain, nearest_distance,activation
         #null_space_velocity,       z_limit更改
         
         
@@ -318,7 +350,115 @@ class KinematicsMonitor(Node):
         #         jacobian_pinv
         #     )
         # )
-        z_limit,secondary_gain,distance_to_limit = (
+        # ==========================================
+        # 约束逆运动学 Constrained IK 对比实验
+        #
+        # 实验目的：
+        # 比较以下三种关节速度求解方式：
+        #
+        # 1. MP伪逆：
+        #    q_dot_mp = J^+ * x_dot_des
+        #
+        # 2. MP结果直接限幅：
+        #    q_dot_clip = clip(q_dot_mp)
+        #
+        # 3. 带边界约束的线性最小二乘：
+        #
+        #    min ||J*q_dot - x_dot_des||^2
+        #
+        #    subject to:
+        #    -q_dot_max <= q_dot <= q_dot_max
+        #
+        # ==========================================
+
+
+        # ------------------------------------------
+        # 关节速度上下限
+        #
+        # 每个关节最大角速度：
+        # |q_dot_i| <= 0.05 rad/s
+        # ------------------------------------------
+        q_dot_max = np.array([
+            0.05,
+            0.05,
+            0.05
+        ])
+        # MP无约束解
+        # 保存MP解作为实验基准
+        q_dot_mp_test = q_dot_mp.copy()
+        # MP解实际产生的末端速度
+        x_dot_mp_test = (
+            jacobian @ q_dot_mp_test
+        )
+        # MP任务空间速度误差
+        error_mp = np.linalg.norm(
+            x_dot_mp_test - desired_velocity
+        )
+        #MP解直接限幅
+        # clip只是对已经得到的MP解逐元素截断，并不会重新优化任务空间误差。
+        q_dot_clip = np.clip(
+            q_dot_mp_test,
+            -q_dot_max,
+            q_dot_max
+        )
+        # 限幅后的实际末端速度
+        x_dot_clip = (
+            jacobian @ q_dot_clip
+        )
+        error_clip = np.linalg.norm(
+            x_dot_clip - desired_velocity
+        )
+        # ------------------------------------------
+        # 边界最小二乘法
+        # 首次约束逆运动学实验
+        # ------------------------------------------
+        # 求解：
+        #
+        # min ||J*q_dot - x_dot_des||^2
+        #
+        # subject to（受限于）:
+        #
+        # -q_dot_max <= q_dot <= q_dot_max
+        #
+        # 与直接clip不同：
+        # lsq_linear会在速度限制范围内重新寻找
+        # 任务空间误差尽可能小的关节速度。
+        result = lsq_linear(
+            jacobian,
+            desired_velocity,
+            bounds=(
+                -q_dot_max,
+                q_dot_max
+            )
+        )
+        # 约束最小二乘得到的关节速度
+        q_dot_constrained = result.x
+        # 实际产生的末端速度
+        x_dot_constrained = (
+            jacobian @ q_dot_constrained
+        )
+        #约束误差
+        error_constrained = np.linalg.norm(
+            x_dot_constrained
+            - desired_velocity
+        )
+        # 不等式一般形式:
+        # A @ q_dot <= b
+        n = jacobian.shape[1]
+        I_n = np.eye(n)
+        #拼接上限  下限 到一个矩阵A
+        A = np.vstack([
+            I_n,
+            -I_n
+        ])
+        b = np.concatenate([
+            q_dot_max,
+            q_dot_max
+        ])
+        Aq = A @ q_dot_constrained
+
+
+        z_limit,secondary_gain,distance_to_limit, activation = (
             self.calculate_joint_limit(q)
         )
         
@@ -516,103 +656,103 @@ class KinematicsMonitor(Node):
             x_dot_total_B - desired_velocity
         )
 
-        # ==========================================
-        # 严格任务优先级实验
-        # ==========================================
+        # # ==========================================
+        # # 严格任务优先级实验
+        # # ==========================================
 
-        #一级任务 
-        q_dot_task1 = q_dot_mp
-        N1 = (
-            np.eye(jacobian.shape[1])
-            - jacobian_pinv @ jacobian
-        )
-        #二级任务：控制第三关节速度
-        J2 = np.array([
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0, 1.0]
-        ])
-        x_dot_2_desired = np.array([
-            0.2,    # q2_dot
-            -0.2     # q3_dot
-        ])
-        #计算残差 r    r2 = x_dot_2-J_2*q_dot_1
-        secondary_residual = (
-            x_dot_2_desired
-            - J2 @ q_dot_task1
-        )
-        #错误实验   直接使用 J2 的伪逆求解二级任务，再通过 N1 投影
-        #预测 结论  一级任务不被破坏 二级任务未达成   
-        y_wrong = (
-            np.linalg.pinv(J2)
-            @ secondary_residual
-        )
-        q_dot_task2_wrong = (
-            N1 @ y_wrong
-        )
-        q_dot_total_wrong = (
-            q_dot_task1
-            + q_dot_task2_wrong
-        )
-        task1_wrong = (
-            jacobian @ q_dot_total_wrong
-        )
-        task2_wrong = (
-            J2 @ q_dot_total_wrong
-        )
+        # #一级任务 
+        # q_dot_task1 = q_dot_mp
+        # N1 = (
+        #     np.eye(jacobian.shape[1])
+        #     - jacobian_pinv @ jacobian
+        # )
+        # #二级任务：控制第三关节速度
+        # J2 = np.array([
+        #     [0.0, 1.0, 0.0],
+        #     [0.0, 0.0, 1.0]
+        # ])
+        # x_dot_2_desired = np.array([
+        #     0.2,    # q2_dot
+        #     -0.2     # q3_dot
+        # ])
+        # #计算残差 r    r2 = x_dot_2-J_2*q_dot_1
+        # secondary_residual = (
+        #     x_dot_2_desired
+        #     - J2 @ q_dot_task1
+        # )
+        # #错误实验   直接使用 J2 的伪逆求解二级任务，再通过 N1 投影
+        # #预测 结论  一级任务不被破坏 二级任务未达成   
+        # y_wrong = (
+        #     np.linalg.pinv(J2)
+        #     @ secondary_residual
+        # )
+        # q_dot_task2_wrong = (
+        #     N1 @ y_wrong
+        # )
+        # q_dot_total_wrong = (
+        #     q_dot_task1
+        #     + q_dot_task2_wrong
+        # )
+        # task1_wrong = (
+        #     jacobian @ q_dot_total_wrong
+        # )
+        # task2_wrong = (
+        #     J2 @ q_dot_total_wrong
+        # )
 
-        #正确方法
-        #一级任务后真正可以使用雅可比
-        J2_projected = J2 @ N1
-        #解J_2*N_1*y=r_2
-        y_task2 = (
-            np.linalg.pinv(J2_projected)
-            @ secondary_residual
-        )
-        #二级修正
-        q_dot_task2_correct = (
-            N1 @ y_task2
-        )
-        #正确的关节总速度
-        q_dot_total_task2 = (
-            q_dot_task1
-            + q_dot_task2_correct
-        )
-        task1_correct = (
-            jacobian @ q_dot_total_task2
-        )
-        task2_achieved = (
-            J2 @ q_dot_total_task2
-        )
-        task2_error = (
-            x_dot_2_desired
-            - task2_achieved
-        )
-        task2_error_norm = np.linalg.norm(
-            task2_error
-        )
+        # #正确方法
+        # #一级任务后真正可以使用雅可比
+        # J2_projected = J2 @ N1
+        # #解J_2*N_1*y=r_2
+        # y_task2 = (
+        #     np.linalg.pinv(J2_projected)
+        #     @ secondary_residual
+        # )
+        # #二级修正
+        # q_dot_task2_correct = (
+        #     N1 @ y_task2
+        # )
+        # #正确的关节总速度
+        # q_dot_total_task2 = (
+        #     q_dot_task1
+        #     + q_dot_task2_correct
+        # )
+        # task1_correct = (
+        #     jacobian @ q_dot_total_task2
+        # )
+        # task2_achieved = (
+        #     J2 @ q_dot_total_task2
+        # )
+        # task2_error = (
+        #     x_dot_2_desired
+        #     - task2_achieved
+        # )
+        # task2_error_norm = np.linalg.norm(
+        #     task2_error
+        # )
 
-        # ==========================================
-        # Task 1 + Task 2 完成后的剩余零空间
-        # ==========================================
-        #J2^——伪逆
-        J2_projected_pinv = np.linalg.pinv(
-            J2_projected
-        )
-        #N2 =N1-J2^——伪逆*J2^——  剩余部分N2=一级剩余空间-二级占用空间  同时不影响 Task 1 和 Task 2 的所有关节运动。
-        N2 = (
-            N1
-            - J2_projected_pinv @ J2_projected
-        )
-        N2_norm = np.linalg.norm(N2)  #对N2 求范数  证明已经无自由度
+        # # ==========================================
+        # # Task 1 + Task 2 完成后的剩余零空间
+        # # ==========================================
+        # #J2^——伪逆
+        # J2_projected_pinv = np.linalg.pinv(
+        #     J2_projected
+        # )
+        # #N2 =N1-J2^——伪逆*J2^——  剩余部分N2=一级剩余空间-二级占用空间  同时不影响 Task 1 和 Task 2 的所有关节运动。
+        # N2 = (
+        #     N1
+        #     - J2_projected_pinv @ J2_projected
+        # )
+        # N2_norm = np.linalg.norm(N2)  #对N2 求范数  证明已经无自由度
 
-        task1_N2_residual = np.linalg.norm(    #J1N2  不会破坏一级任务
-            jacobian @ N2
-        )
-        task2_N2_residual = np.linalg.norm(    #J2N2  不会破坏二级任务
-            J2 @ N2
-        )
-        #增加三级任务 验证任务存在但机器人没有自由度执行任务
-        q_dot_task3 = N2 @ z_sing
+        # task1_N2_residual = np.linalg.norm(    #J1N2  不会破坏一级任务
+        #     jacobian @ N2
+        # )
+        # task2_N2_residual = np.linalg.norm(    #J2N2  不会破坏二级任务
+        #     J2 @ N2
+        # )
+        # #增加三级任务 验证任务存在但机器人没有自由度执行任务
+        # q_dot_task3 = N2 @ z_sing
 
         # ==========================================
         # 严格任务优先级递归实验
@@ -710,18 +850,57 @@ class KinematicsMonitor(Node):
             # f'||J1*N2|| = {task1_N2_residual:.10e}\n'
             # f'||J2*N2|| = {task2_N2_residual:.10e}\n' 
             # f'Nz_sing = {np.linalg.norm(q_dot_task3):.10e}\n'
-            '===== 二级不可行任务实验 =====\n'
-            f'期望二级任务关节速度: {x_dot_2_desired}\n'
-            f'第二个有效雅可比:\n{J2_projected}\n'
-            f'第二个有效雅可比的秩: '
-            f'{np.linalg.matrix_rank(J2_projected)}\n'
-            f'总关节速度: {q_dot_total_task2}\n'
-            f'一级任务达成的: '
-            f'{jacobian @ q_dot_total_task2}\n'
-            f'二级任务达成的: {task2_achieved}\n'
-            f'二级任务残差: {task2_error}\n'
-            f'||二级任务残差||: '
-            f'{task2_error_norm:.10e}'
+            # '===== 二级不可行任务实验 =====\n'
+            # f'期望二级任务关节速度: {x_dot_2_desired}\n'
+            # f'第二个有效雅可比:\n{J2_projected}\n'
+            # f'第二个有效雅可比的秩: '
+            # f'{np.linalg.matrix_rank(J2_projected)}\n'
+            # f'总关节速度: {q_dot_total_task2}\n'
+            # f'一级任务达成的: '
+            # f'{jacobian @ q_dot_total_task2}\n'
+            # f'二级任务达成的: {task2_achieved}\n'
+            # f'二级任务残差: {task2_error}\n'
+            # f'||二级任务残差||: '
+            # f'{task2_error_norm:.10e}'
+            # '===== 严格任务优先级递归实验 =====\n'
+            # f'q_dot_0: {q_dot_0}\n'
+            # f'N0:\n{N0}\n'
+            # '\n--- 一级任务 ---\n'
+            # f'r1: {r1}\n'
+            # f'J1_bar:\n{J1_bar}\n'
+            # f'q_dot_1: {q_dot_1}\n'
+            # f'||q_dot_1 - q_dot_mp||: '
+            # f'{np.linalg.norm(q_dot_1 - q_dot_mp):.10e}\n'
+            # '\n--- 二级任务 ---\n'
+            # f'J2_bar:\n{J2_bar}\n'
+            # f'r2: {r2}\n'
+            # f'q_dot_2: {q_dot_2}\n'
+            # f'Task 1 achieved: {J1 @ q_dot_2}\n'
+            # f'Task 2 achieved: {J2 @ q_dot_2}\n'
+            # '\n--- 剩余零空间 ---\n'
+            # f'N2:\n{N2}\n'
+            # f'||J1*N2||: '
+            # f'{np.linalg.norm(J1 @ N2):.10e}\n'
+            # f'||J2*N2||: '
+            # f'{np.linalg.norm(J2 @ N2):.10e}'
+            # f'Distance: {distance_to_limit}\n'
+            # f'Joint limit activation: {activation}\n'
+            '===== 约束逆运动学实验 =====\n'
+            f'MP q_dot: {q_dot_mp_test}\n'
+            f'MP achieved: {x_dot_mp_test}\n'
+            f'MP error: {error_mp:.10e}\n'
+            f'Clip q_dot: {q_dot_clip}\n'
+            f'Clip achieved: {x_dot_clip}\n'
+            f'Clip error: {error_clip:.10e}\n'
+            f'Constrained q_dot: {q_dot_constrained}\n'
+            f'Constrained achieved: {x_dot_constrained}\n'
+            f'Constrained error: {error_constrained:.10e}'
+            '===== 二次规划约束不等式 =====\n'
+            f'A:\n{A}\n'
+            f'b: {b}\n'
+            f'A @ q_dot_constrained: '
+            f'{Aq}\n'
+            f'Aq <= b: {Aq <= b}'
         )
 
     def print_result(self):
@@ -738,7 +917,7 @@ class KinematicsMonitor(Node):
                 f'End-effector velocity = '
                 f"{result['end_effector_velocity']} m/s\n"
                 f"Velocity source: {result['velocity_source']}\n"
-            )
+        )
 
         self.get_logger().info(
             '\n'
